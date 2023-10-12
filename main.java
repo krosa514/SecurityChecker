2ea6d49d1932893c95ffae18b89c6c60aef1240e
Dylan-Shu <78428141+Dylan-Shu@users.noreply.github.com>
2023-10-11 20:55:08 -0400

    10/11 Classes and empty functions

---
+++ main.java
@@ -0,0 +1,75 @@
+// Vulnerability Vigil
+import java.util.ArrayList;
+
+
+class HealthChecker {
+    // Main class
+    public static void main() {
+
+    }
+    // Select scanners
+    public void selectScanners() {
+
+    }
+    // Keeps an array of scanners for the user to choose
+    ArrayList<Scanners> Scanners = new ArrayList<Scanners>();
+    // Runs the scanners that were chosen by the user
+    public void runScanners(Scanners) {
+
+    }
+
+}
+
+class Scanner {
+    // Different types of scans that will be performed
+    public void scan() {
+
+    }
+
+    public void VirusScanner() {
+
+    }
+
+    public void ApacheScanner() {
+
+    }
+
+    public void FirewallScanner() {
+
+    }
+}
+
+class AttackTesting {
+    // Testing with different attacks to check security of server/website
+}
+
+class SystemChecks {
+    // Keeps system in check for future warnings and problems
+    public void ConfigCheck() {
+
+    }
+
+    public void SystemUpdate() {
+
+    }
+
+    public void PasswdCheck() {
+
+    }
+}
+
+class Utilities {
+    // Functionalities of the program
+}
+
+class Report {
+    // Generate report after the scan is complete
+    public void generateReport() {
+
+    }
+
+    public void reportToHTML() {
+
+    }
+}
+

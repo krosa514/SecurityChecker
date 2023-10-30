9f46fad5f62a0c664cfec33ca47ab496a4a619f9
Dylan-Shu <78428141+Dylan-Shu@users.noreply.github.com>
2023-10-30 09:59:07 -0400

    New commit

--- VirusScanner.java
+++ VirusScanner.java
@@ -2,23 +2,14 @@ public class VirusScanner implements Scanner {
 
     @Override
     public Report Scan() {
-        ScanThread thread = new ScanThread();
-        thread.start();
-        thread.join();
-        String result = thread.cmd_result;
-        Report report = new Report();
-        return report;
+
     }
 }
 
 class ScanThread extends Thread {
     
-    protected String cmd_result;
-    
     @Override
     public void run() {
-        Process proc = new Process("clamscan -i -v -r --log=logfile.txt /");
-        proc.start();
-        // Add line to get stdout from log file
+
     }
 }

a25c4c37b17c60ebb7957768f56133dd5f3597f5
TylerB0 <tbonds1@pride.hofstra.edu>
2023-11-08 13:50:20 -0500

    Update to files Dr. Fu commented on

--- Scanner.java
+++ Scanner.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+
 public interface Scanner {
-    public Report Scan();
+    public Report Scan() throws IOException;
+    public void start(); //call the Scan when you run using a thread, set a report data attribute once report is generated
+    public void join();
+    public String getName();
+    public Report getReport();
 }
--- main.java
+++ main.java
@@ -1,14 +1,93 @@
 // Vulnerability Vigil
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 class VulnerabilityVigil {
-    // Main class
-    public static void main() {
-    
+    /*enum scanType {
+        FULL,
+        QUICK,
+        CUST
+    } */
+
+    /**
+     * Interact with user and return an array list of scanners
+     * @return
+     */
+    protected ArrayList<Scanner> selectScanner(){
+        ArrayList<Scanner> allScanners = new ArrayList<>();
+        allScanners.add(new fullScan());
+        //add more
+
+        ArrayList<Scanner> selectedScanners = new ArrayList<>();
+        System.out.println("Type numeric values separated by spaces to select scanners: ");
+        for(int i=0; i<allScanners.size(); i++){
+            System.out.println(i + ": " + allScanners.get(i).getName();)
+        }
+
+        String line = null; //System.in. read a line
+        String words = line.split();
+        for(int i=0; i<words.size(); i++){
+            int idx =Integer.parseInt( words[i] );
+            selectedScanners.add(allScaners.get(idx));
+        }
+        return selectedScanners;
     }
-    // Select scanners
-    public void selectScanners() {
+
+    /**
+     * Stub function for main
+     */
+    protected void main2(){
+        //1. get the list of scanners to run
+        ArrayList<Scanner> arrScanner = selectScanner();
+
+        //2. run each scanner
+        for(int i=0; i<arrScanner.size(); i++){
+            arrScanner.get(i).start();
+        }
+        ArrayList<Report> arrReports = new ArrayList<>();
+        for(int i=0; i<arrScanner.size(); i++){
+            arrScanner.get(i).join();
+            Report report = arrScanner.get(i).getReport();
+            arrReports.add(report);
+        }
+        // NEED a report function which writes an array of reports as one HTML 
+        //3. generate the report
+        System.out.println("REPORT is generated and located at: ");
+    }
+    
+    // Main class
+    public static void main(String[] args) {
+        main2();
+        
+        // ignore below this was my test code
+        scanType urType = scanType.FULL;
+        switch (urType) {
+            case FULL:
+                fullScan Test1 = new fullScan();
+                try {
+                    Test1.Scan();
+                } catch (IOException e) {
+                    return;
+                }
+                System.out.print(Test1.getOutput());
+                break;
+            case QUICK:
+                System.out.println("The quickscan.");
+                break;
+            case CUST:
+                System.out.println("The customscan.");
+                break;
+            default:
+                break;
+        }
 
     }
+
+    // Select scanners
+    public void selectScanners() {
+        return;
+    }
     // Keeps an array of scanners for the user to choose
-    ArrayList<Scanners> Scanners = new ArrayList<Scanners>();
+    // ArrayList<Scanners> Scanners = new ArrayList<Scanners>();
 }

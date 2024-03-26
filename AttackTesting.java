b155280b481e0546bc9699e346f1973792e51796
Dylan-Shu <dshu5@pride.hofstra.edu>
2024-03-26 12:04:53 -0400

    removed unnecessary files

--- AttackTesting.java
+++
@@ -1,3 +0,0 @@
-public interface AttackTesting {
-    public Report Test();
-}
--- SystemCheck.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public interface SystemCheck {
-    public Report Check() throws IOException;
-    public void start();
-    public void join();
-    public String getName();
-    public Report getReport();
-}
